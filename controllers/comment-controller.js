8af1f03138834ec1b7d90833c2b1d637069b6ce5
Liz A <lizethariasc14@gmail.com>
2021-08-14 09:53:16 -0700

    create routes

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -7,20 +7,20 @@ const commentController = {
     Comment.create(body)
       .then(({ _id }) => {
         return Pizza.findOneAndUpdate(
-            { _id: params.pizzaId },
-            { $push: { comments: _id } },
-            { new: true }
-          );
-        })
-        .then(dbPizzaData => {
-          if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-          }
-          res.json(dbPizzaData);
-        })
-        .catch(err => res.json(err));
-    },
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
 
   // remove comment
   removeComment({ params }, res) {
