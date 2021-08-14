b14be0840d3ec26de9594c6b14e10cd5486c8a23
Liz A <lizethariasc14@gmail.com>
2021-08-14 12:40:30 -0700

    integrated front end

--- models/Comment.js
+++ models/Comment.js
@@ -18,31 +18,32 @@ const ReplySchema = new Schema(
       type: Date,
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
-       }
-    },
-    { 
-      toJSON: {
-        getters: true
-      }
     }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
 );
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String
+    },
+    commentBody: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
   },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: createdAtVal => dateFormat(createdAtVal)
-  },
-  // use ReplySchema to validate data for a reply
-  replies: [ReplySchema]
-},
-{
+  {
     toJSON: {
       virtuals: true,
       getters: true
