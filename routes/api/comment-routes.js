c6e80b259594ee491333059ccc2fea0381ba2e9f
Liz A <lizethariasc14@gmail.com>
2021-08-14 12:22:44 -0700

    comment controller and routes'

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,23 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, 
+        removeComment,
+        addReply,
+        removeReply
+      } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
+
+// put route to handle addNewReply() by editing /api/comment/:pizzaId/:commentId
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment)
+
+//delete route to handle removeReply() 
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
